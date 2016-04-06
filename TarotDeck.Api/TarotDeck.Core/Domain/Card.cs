8d9bc9e0d6e1ee95a1cd1837085cac5d3c3e832e
Rebecca Mooney <astrakiseki@gmail.com>
2016-04-05 18:38:55 -0700

    Made the .api project, wrote up the Card class.

---
+++ TarotDeck.Api/TarotDeck.Core/Domain/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarotDeck.Core.Domain
+{
+    public enum CardNumber
+    {
+        // Intended to go 0 - 22 to allow for the Major Arcana
+    }
+    public class Card
+    {
+        public string Suit { get; set; }
+        public string Meaning { get; set; }
+        public bool Reversed { get; set; }
+        public string ReversedMeaning { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public CardNumber CardNumber { get; set; }
+    }
+}
