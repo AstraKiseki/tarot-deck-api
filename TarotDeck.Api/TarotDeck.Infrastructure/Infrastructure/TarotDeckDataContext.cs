cb9d6adc86d7e706d9532ab916233f7b0fdb5e47
Rebecca Mooney <astrakiseki@gmail.com>
2016-04-21 07:36:37 -0700

    /add AutoMapper, Datacontext

---
+++ TarotDeck.Api/TarotDeck.Infrastructure/Infrastructure/TarotDeckDataContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TarotDeck.Core.Domain;
+
+public class TarotDeckDataContext : DbContext
+{
+    public TarotDeckDataContext() : base("TarotDeck")
+    {
+        var ensureDllIsCopied = System.Data.Entity.SqlServer.SqlProviderServices.Instance;
+    }
+
+    //SQL Tables
+    public IDbSet<Deck> Decks { get; set; }
+    public IDbSet<Card> Cards { get; set; }
+
+    //Model Relationships
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    {
+        //Deck
+        modelBuilder.Entity<Deck>()
+                    .HasMany(d => d.Cards)
+                    .WithRequired(c => c.Deck)
+                    .HasForeignKey(c => c.CardId);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
+}
