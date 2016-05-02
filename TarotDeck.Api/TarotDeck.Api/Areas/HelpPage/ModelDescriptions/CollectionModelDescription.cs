36c2a144f5210ff958fca5bda122e4c366216c7e
Rebecca Mooney <astrakiseki@gmail.com>
2016-05-02 10:46:13 -0700

    Need a new refactoring, switching up the api framework.

---
+++ TarotDeck.Api/TarotDeck.Api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace TarotDeck.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ TarotDeck.Api/TarotDeck.Api/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TarotDeck.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
